7800d745d99583a70eee14d88d10d940d4c597c6
tycompton <tyrone.compton@gmail.com>
2021-03-05 17:49:21 +0000

    AppBar installed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import Home from './components/splash/home.js';
 import Info from './components/info/info.js';
 import Map from './components/map/map.js';
-// import Nav from './components/nav/nav.js';
+import AppBar from './components/AppBar/MenuAppBar';
 import Footer from './components/footer/footer.js';
 
 function App() {
   return (
     <div className="container">
+      <AppBar />
       <Home />
       <Info />
       <Map />

7426501edf19670d91ab36f7c5f6db2b023edac1
tycompton <tyrone.compton@gmail.com>
2021-03-12 19:21:27 +0000

    map responsive

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-copyright">Copyright &copy; Old Chigwellians' Club MMXXI</div>
+      <div className="footer-copyright">Copyright &copy; Old Chigwellians' Club 2021</div>
       <div className="footer-socials">  </div>
     </footer>
   );
